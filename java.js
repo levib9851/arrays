ff92003fcba2e7f3cf493bb0fe604e33c242d693
levib9851 <Butlerl@g.crookcountyschools.org>
2021-03-17 13:31:06 -0700

    i did lab 51

---
+++ java.js
@@ -0,0 +1,11 @@
+'use strict'
+
+let favriteBooks = ['dark life', 'trials of apollo book 2', 'the heros of olympus book 5 ', 'the heros of olympus book 2', 'the hobbit', 'magnus chase and the gods of asgard book 1', 'magnus chase and the gods of asgard book 3', 'sting', 'the khane cronicles book 1', 'the khane cronicles book 3']
+
+console.log (favriteBooks[0]);
+console.log (favriteBooks[9]);
+console.log (favriteBooks[0, 9]);
+
+let numbers = [1,2,3,4,5]
+console.log (numbers[1 + 2]);
+console.log (numbers[0 * 1 * 4]);
